d0d3c7b404d21e5e638a056e0180226c0216d1e8
Sara Amer <saraamersaad8@gmail.com>
2021-05-28 21:34:01 +0200

    Restaurants Header

---
+++ src/components/Restaurants/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Header.css"
+class Header extends React.Component{
+    render(){
+        return (
+          <div id = "container">
+              <span id="logo">
+                  talabat
+              </span>
+     
+              
+          </div>
+        
+        )
+    }
+}
+export default Header
