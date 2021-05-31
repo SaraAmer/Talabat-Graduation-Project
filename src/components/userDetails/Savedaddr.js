68c5db480df584396855957740bd278b01de1bd1
RaghdaMadiane <raghdamadiane@gmail.com>
2021-05-31 03:27:06 +0200

    login

--- src/components/userDetails/Savedaddr.js
+++ src/components/userDetails/Savedaddr.js
@@ -1,11 +1,34 @@
 import React from 'react'
 import Accountinfo from './Account info'
+ import GoogleMap from 'google-map-react';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Link
 } from "react-router-dom"
+const mapStyles = {
+      width: '100%',
+      height: '100%'
+    }
+    
+    const markerStyle = {
+      height: '50px',
+      width: '50px',
+      marginTop: '-50px'
+    }
+    
+    const imgStyle = {
+      height: '100%'
+    }
+    
+    
+    const Marker = ({ title }) => (
+      <div style={markerStyle}>
+        <img style={imgStyle} src="https://res.cloudinary.com/og-tech/image/upload/s--OpSJXuvZ--/v1545236805/map-marker_hfipes.png" alt={title} />
+        <h3>{title}</h3>
+      </div>
+    );
 class Savedaddr extends React.Component {
 	 constructor() {
     super();
@@ -52,13 +75,71 @@ class Savedaddr extends React.Component {
 						</div>
 						<div className="col-md-9 border">
 							<div className="card-body ">
-								
+								<button type="button" className="btn btn-success"  style={{marginLeft:"400px",marginTop:"100px"}} data-toggle="modal" data-target="#exampleModal3">
+								Add Address
+								</button>
 					
 							</div>
 						</div>
 				    </div>
+				</div>
+				<div className="modal fade" id="exampleModal3" tabIndex="-1" aria-labelledby="exampleModalLabel3" aria-hidden="true">
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h5 className="modal-title" id="exampleModalLabel3">Add New Address</h5>
+							<button type="button" className="close border-0" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;
+								</span>
+							</button>
+						</div>
+					    <div className="modal-body "style={{width:"400px",height:"400px"}}>
+							  <div >
+            <GoogleMap
+              style={mapStyles}
+              bootstrapURLKeys={{ key: 'GOOGLE_MAPS_API_KEY' }}
+              center={{ lat: 5.6219868, lng: -0.1733074 }}
+              zoom={14}
+            >
+              <Marker
+              title={'Current Location'}
+              lat={5.6219868}
+              lng={-0.1733074}
+            >
+              </Marker>
+            </GoogleMap>
+          </div>
+					    </div>
+					    <div className="modal-footer">
+							
+							<button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal4">Confirm 
+							</button>
+						</div>
 					</div>
-	       </div>
+				</div>
+			</div>
+			<div className="modal fade" id="exampleModal4" tabIndex="-1" aria-labelledby="exampleModalLabel4" aria-hidden="true">
+				<div className="modal-dialog">
+					<div className="modal-content">
+						<div className="modal-header">
+							<h5 className="modal-title" id="exampleModalLabel4">Add New Address</h5>
+							<button type="button" className="close border-0" data-dismiss="modal" aria-label="Close">
+								<span aria-hidden="true">&times;
+								</span>
+							</button>
+						</div>
+					    <div className="modal-body">
+							...
+					    </div>
+					    <div className="modal-footer">
+							
+							<button type="button" className="btn btn-primary" >Save Address
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+	        </div>
 			
 			
 			
