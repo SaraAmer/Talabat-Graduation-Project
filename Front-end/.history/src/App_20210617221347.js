2d46ebfdb8b65bc0744579dba1415ac48e0ad318
walaaElbasha <walaaelbasha40@yahoo.com>
2021-06-17 23:35:01 +0200

    dependancy solved

---
+++ Front-end/.history/src/App_20210617221347.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import "./App.css";
+import Header from './components/layouts/Header';
+import Footer from './components/layouts/Footer';
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Pagination from '@material-ui/lab/Pagination';
+import { makeStyles } from '@material-ui/core/styles';
+import AllRestaurants from './components/AllRestaurants'
+import PaginationMenu from '../src/components/restaurants-client/pagination';
+import TeamHome from './components/talabatTeam/Home'
+import Home from './components/Home'
+import Register from './components/Register';
+import RestaurantMenu from './components/RestaurantMenu';
+function App(){
+
+
+  return (
+    <div className="App">
+     <Header/>
+     {/* <RestaurantMenu/> */}
+     {/* <Register/> */}
+  {/* <TeamHome/> */}
+  <Home/>
+	<Footer/>
+    </div>
+  );
+  }
+
+
+export default App;
+
