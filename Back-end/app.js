97e1673be5dacb3dbe6b4b20a794768282b3951c
walaaElbasha <walaaelbasha40@yahoo.com>
2021-06-23 01:46:06 +0200

    Restaurant Api's Complete

--- Back-end/app.js
+++ Back-end/app.js
@@ -10,25 +10,26 @@ const User = require("./api//models/user");
 // ******************************************
 const restaurantsRoutes = require("./api/routers/restaurants");
 const userRoutes = require("./api/routers/user");
+//************ for upload img
+const fs = require('fs');
+const path = require('path');
+require('dotenv/config');
+var multer = require('multer');
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }));
+ // parse application/json 
+app.use(bodyParser.json());
+app.use('/uploads', express.static('uploads'));
 
-// **********************************
-mongoose.connect(
-  "mongodb://localhost:27017/",
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  function (connectErr, client) {
-    const db = client.db("talabat");
-    // console.log("hi");
-    // client.close();
-  }
-);
-mongoose.connection.on("connected", () => {
-  console.log("connected");
-});
+mongoose.connect('mongodb+srv://eithar:123@cluster0.jg0og.mongodb.net/Talabat?retryWrites=true&w=majority')
+    .then(result => {
+        app.listen(4000);
+        console.log(result);
 
-mongoose.connection.on("error", (err) => {
-  console.log("error");
-});
-// **************************
+    })
+    .catch(err => {
+        console.log(err);
+    });
 var Schema = mongoose.Schema;
 console.log(
   "********************************************************************"
@@ -47,6 +48,36 @@ app.post("/hello", (req, res) => {
   const name = req.body.name;
   res.send({ message: `welcome ${name}` });
 });
+
+
+//If reaches this line, then there is an error
+app.use((req, res, next) => {
+const error = new Error('Not Found');
+error.status = 404;
+next(error);
+});
+//handel error:
+app.use((error, req, res, next) => {
+res.status(error.status || 500);
+res.json({
+error: {
+message: error.message
+}
+});
+});
+
+//**************img  upload************* */
+ 
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+ 
+var upload = multer({ storage: storage });
 //*********************
 // app.post("/signup", (req, res, next) => {
 //   const email = req.body.email;
